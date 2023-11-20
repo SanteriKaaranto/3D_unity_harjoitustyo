b9859c1de397de3c791476b328cc90e4ed877cae
Santeri <santeri.kaaranto@gmail.com>
2023-11-20 15:41:19 +0200

    Respawn

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private Rigidbody PlayerRB;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Kun pelaaja osuu respawn collider objectiin, viedään pelaaja respawn alueelle.
+        player.transform.position = respawnPoint.transform.position;
+        // Asetetaan pelaajan nopeus nollaan
+        PlayerRB.velocity = Vector3.zero;
+    }
+}
