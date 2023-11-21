8c960dc58b0a36503662e5658087738c97ff9c49
Santeri <santeri.kaaranto@gmail.com>
2023-11-21 13:37:17 +0200

    Continued the map, teleporter

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform TeleportEnd;
+    [SerializeField] private Rigidbody PlayerRB;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Kun pelaaja osuu respawn collider objectiin, viedään pelaaja respawn alueelle.
+        player.transform.position = TeleportEnd.transform.position;
+        // Asetetaan pelaajan nopeus nollaan
+        PlayerRB.velocity = Vector3.zero;
+    }
+}
